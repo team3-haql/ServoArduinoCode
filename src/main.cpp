3681e52db631d9cdbe58a0511467a5ea2ca9236c
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-03-19 19:07:38 -0600

    fixed it?

--- src/main.cpp
+++ src/main.cpp
@@ -9,25 +9,25 @@
 #define L 1
 #define W 1
 
-Servo servos[SERVO_COUNT];  // Create servos
+Servo g_servos[SERVO_COUNT];  // Create servos
 
 void setup() {
 	Serial.begin(9600);  // Start serial communication
-	const int servoPins[SERVO_COUNT] = {9, 2, 3, 4};
+	const int servoPins[SERVO_COUNT] = {9, 10, 6, 5};
 	for (int i = 0; i < SERVO_COUNT; i++) { // Attach the servo to the defined pin
-		servos[i].attach(servoPins[i]);
-		servos[i].write(START_ANGLE);
+		g_servos[i].attach(servoPins[i]);
+		g_servos[i].write(START_ANGLE);
 	}
 	Serial.println("Enter an angle (0-180):");
 }
 
-void writeToServos(const float val) {
+void writeToServos(const int val) {
 	for (int i = 0; i < SERVO_COUNT; i++) {
 		// If i < 2 is true sign is -1 otherwise its 1.
 		// Prevents branching
-		float sign = 1.0 - (2.0*static_cast<float>(i < 2));
+		// float sign = 1.0 - (2.0*static_cast<float>(i < 2));
 
-		servos[i].write((val * 180.0 / PI));
+		g_servos[i].write(val);
 	}
 }
 
@@ -36,9 +36,7 @@ void loop() {
     if (Serial.available()>0) {
         String received = Serial.readStringUntil('\n');  // Read until newline
         int turn_angle = received.toInt();  // Convert to integer
-        R = (float)turn_angle * PI / 180;
-      
-        float radians = atan((L/ (R - (W/2))));
-        writeToServos(radians);
+        Serial.println(PI);
+        writeToServos(turn_angle);
     }   
 }
