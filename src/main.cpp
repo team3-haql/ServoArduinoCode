44f92cb3885952da37e32be0e9937f840eaf1794
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-03-19 19:20:46 -0600

    removed stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,31 +4,27 @@
 #include <math.h>
 
 #define SERVO_COUNT 4
-#define START_ANGLE 90
-
-#define L 1
-#define W 1
+#define START_ANGLE 0
 
 Servo g_servos[SERVO_COUNT];  // Create servos
+const int g_servoPins[SERVO_COUNT] = {9, 10, 6, 5}; // 
+int g_L = 1;
+int g_W = 1;
 
 void setup() {
 	Serial.begin(9600);  // Start serial communication
-	const int servoPins[SERVO_COUNT] = {9, 10, 6, 5};
 	for (int i = 0; i < SERVO_COUNT; i++) { // Attach the servo to the defined pin
-		g_servos[i].attach(servoPins[i]);
+		g_servos[i].attach(g_servoPins[i]);
 		g_servos[i].write(START_ANGLE);
 	}
 	Serial.println("Enter an angle (0-180):");
 }
 
 void writeToServos(const int val) {
-	for (int i = 0; i < SERVO_COUNT; i++) {
-		// If i < 2 is true sign is -1 otherwise its 1.
-		// Prevents branching
-		// float sign = 1.0 - (2.0*static_cast<float>(i < 2));
-
-		g_servos[i].write(val);
-	}
+	g_servos[0].write(val);
+	g_servos[1].write(val);
+	g_servos[2].write(val);
+	g_servos[3].write(val);
 }
 
 void loop() {
@@ -36,7 +32,7 @@ void loop() {
     if (Serial.available()>0) {
         String received = Serial.readStringUntil('\n');  // Read until newline
         int turn_angle = received.toInt();  // Convert to integer
-        Serial.println(PI);
+
         writeToServos(turn_angle);
     }   
 }
