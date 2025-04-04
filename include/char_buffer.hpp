7767827ec9530fc8aab246398ed1c926da091c12
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-04 13:43:13 -0600

    fixed issue of data being read early

--- include/char_buffer.hpp
+++ include/char_buffer.hpp
@@ -8,15 +8,30 @@ typedef unsigned char BufferSize;
 static_assert(MAX_TYPE_SIZE(BufferSize) >= BUFFER_SIZE-1, "BufferIterator is too small!");
 
 /**
+ * @brief Whether or not serial is empty
+ * 
+ * @return true 
+ * @return false 
+ */
+bool serialEmpty() {
+    return Serial.available() == 0;
+}
+
+/**
  * @brief Reads from Serial port 
  * 
  * @param buffer 
  */
 void read(char* buffer) {
+    bool done = false;
     BufferSize i = 0;
-    for (; i < BUFFER_SIZE-1 && Serial.available() > 0; i++) {
+    for (; i < BUFFER_SIZE-1; i++) {
+        while(serialEmpty()) { // Wait until more data is available
+            delay(1);
+        }
         char character = Serial.read();
-        if (character == '\n') {
+        if (character == '\n') { // Exit
+            done = true;
             break;
         }
         buffer[i] = character;
