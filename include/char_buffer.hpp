df2542a6416da5c0447c17ea65d9b5d4b71101dd
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-04 12:31:31 -0600

    made char buffer only accept digits

--- include/char_buffer.hpp
+++ include/char_buffer.hpp
@@ -13,16 +13,16 @@ static_assert(MAX_TYPE_SIZE(BufferSize) >= BUFFER_SIZE-1, "BufferIterator is too
  * @param buffer 
  */
 void read(char* buffer) {
-    // Prevents atof from potentially failing
-    buffer[BUFFER_SIZE-1] = '\0';
     // Precompute size of for loop for better performance
     // Assumes Serial.available() does not change while for loop is running.
     const BufferSize size = MIN(BUFFER_SIZE-1, Serial.available());
-    for (BufferSize i = 0; i < size; i++) {
-        buffer[i] = Serial.read();
-        if (buffer[i] == '\n') { // '\n' indicates end of transmission.
-            buffer[i] = '\0'; // Set 'i' to be end of string
-            break;
+    BufferSize i = 0;
+    for (; i < size; i++) {
+        char character = Serial.read();
+        if (!isdigit(character)) {
+            continue;
         }
+        buffer[i] = character;
     } 
+    buffer[i] = '\0';
 }
