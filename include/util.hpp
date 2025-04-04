5c33945b73f48addb405f1d79f4e2ea9ec57fb86
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-04 12:01:51 -0600

    Simplified macro

--- include/util.hpp
+++ include/util.hpp
@@ -11,12 +11,11 @@
     #define LOGLN(x)
 #endif
 
-#define BIT_CAST(type, bits) static_cast<type>((static_cast<type>(0) | (bits >> 8*(4 - sizeof(type)))))
-#define MAX_TYPE_SIZE(type) \
-    BIT_CAST(type, 0xffffffff)*(BIT_CAST(type, 0xffffffff) >= BIT_CAST(type, 0x7fffffff)) + \
-    BIT_CAST(type, 0x7fffffff)*(BIT_CAST(type, 0xffffffff) < BIT_CAST(type, 0x7fffffff))
-
 #define MIN(a, b) a*(b > a) + b*(b <= a)
+#define MAX(a, b) a*(b < a) + b*(b >= a)
+
+#define BIT_CAST(type, bits) static_cast<type>((static_cast<type>(0) | (bits >> 8*(4 - sizeof(type)))))
+#define MAX_TYPE_SIZE(type) MAX(BIT_CAST(type, 0xffffffff), BIT_CAST(type, 0x7fffffff))
 
 #define DEG2RAD(angle) angle*(PI/180.0)
 #define RAD2DEG(angle) angle*(180.0/PI)
