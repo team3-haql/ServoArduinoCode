b6607ed94db8a4f0e9e6f20460209d247962f477
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-06 17:52:30 -0600

    Fixed it, works with real arduino

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,17 @@
 #include "servo.hpp"
 #include "constants.hpp"
 
-#ifdef LOGGING
-FILE fout;
-#endif
-
 void setup() {
 	Serial.begin(9600);  // Start serial communication
 	ralphee::initServos();        // Initializes servos
-
-#ifdef LOGGING
-	// Makes printf work
-	fdev_setup_stream(&fout, ralphee::sput, nullptr, _FDEV_SETUP_WRITE); // cf https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gaf41f158c022cbb6203ccd87d27301226
-	stdout = &fout;
-#endif
 
 	LOG("Enter an angle [-1,1]:\n");
 }
 
 void loop() {
     if (Serial.available() >= MIN_CHARS_IN_MESSAGE) {
-		static char buffer[BUFFER_SIZE];
+		char buffer[BUFFER_SIZE];
+		LOGLN("READ");
 		if (ralphee::read(buffer) < 0) {
 			return;
 		}
@@ -36,9 +27,7 @@ void loop() {
 		// Cast input
         double input = atof(buffer);
 
-		LOG("Input: ");
-		LOG(input);
-		LOG("\n");
+		LOG("Input: "); LOGLN(input);
 
 		// Calculate theta inner and outer
 		double angles[2];
